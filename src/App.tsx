0b21a8967167fcecbbd8ea48c7969a84f6852fc0
Subha Sadhu <subhasadhu5@gmail.com>
2023-10-03 03:09:29 +0530

    Basic layout of the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
+import Select from "./components/Select";
+
+const OPTIONS = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 },
+]
+
 function App() {
-  return <h1>Hi</h1>
+
+  return <><Select options={OPTIONS} /></>
 }
 export default App
